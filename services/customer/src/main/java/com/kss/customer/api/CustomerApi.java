dee9e5d5247d21d4fa6a479a76d566143b3b4b99
Ksspride <ksspride2110@gmail.com>
2024-07-29 23:28:52 +0530

    started the testing the services of e-com application

--- services/customer/src/main/java/com/kss/customer/api/CustomerApi.java
+++ services/customer/src/main/java/com/kss/customer/api/CustomerApi.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/customer")
+@RequestMapping("/api/v1/customers")
 public class CustomerApi {
 
     @Autowired
